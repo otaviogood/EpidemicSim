67c90d1d9fc2c696d4ab823fbf027e9e7217047d
Francesco <FR>
2020-06-13 00:45:11 -0700

    explicit initial memory limit in emscripten build command

--- sim_backends/c_wasm/build.sh
+++ sim_backends/c_wasm/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 OUT_DIR=../../src/generated_wasm
-FLAGS="--js-library lib.js --closure 1 --llvm-lto 1 --profiling --bind -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0 -s ASSERTIONS=1 -O3 -s EXPORT_ES6=1 -s MODULARIZE=1 -s USE_ES6_IMPORT_META=0 -s ALLOW_MEMORY_GROWTH=1"
+FLAGS="--js-library lib.js --closure 1 --llvm-lto 1 --profiling --bind -s WASM=1 -s DISABLE_EXCEPTION_CATCHING=0 -s ASSERTIONS=1 -O3 -s EXPORT_ES6=1 -s MODULARIZE=1 -s USE_ES6_IMPORT_META=0 -s ALLOW_MEMORY_GROWTH=1 -s WASM_MEM_MAX=2000Mb"
 
 emcc $FLAGS -o $OUT_DIR/resident_counter.js resident_counter.cc
 
